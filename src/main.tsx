c8ea961442e4d507e4c341b4d5e46f6163cc8799
guli3550 <parizodabaxtiyorov@gmail.com>
2026-08-25 12:09:06 +0500

    Remove experimental promo enhancer

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from './App.tsx'
 import Admin from './admin/AdminPro.tsx'
 import './index.css'
 import './admin/AdminNaming.css'
-import './admin/promoEnhancer'
 
 if (typeof window !== 'undefined') {
   const webApp = window.Telegram?.WebApp
