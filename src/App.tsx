d4fc1a9b86cb0aad545f1ef7d4e558c8ba273dd1
guli3550 <parizodabaxtiyorov@gmail.com>
2026-09-23 02:13:31 +0500

    feat: add professional PWA install prompt
    * feat: add native PWA install prompt
    
    * feat: add PWA install prompt styles
    
    * feat: mount GULI PWA install prompt
    
    * fix: mount PWA prompt outside conditional header

--- src/App.tsx
+++ src/App.tsx
@@ -65,6 +65,8 @@ import { getSupabase, signOutEverywhere } from "./lib/supabaseClient";
 import { ModernProfileView } from "./components/ModernProfileView";
 import { checkReceiptDelayed, getDeliveryEstimate } from "./utils/delivery";
 import { copyToClipboard } from "./utils/clipboard";
+import { PwaInstallPrompt } from "./components/PwaInstallPrompt";
+import "./components/PwaInstallPrompt.css";
 
 declare global {
   interface Window {
@@ -4516,6 +4518,7 @@ export default function App() {
         </div>
       </header>
       )}
+      <PwaInstallPrompt />
 
       <div
         className="tabSwipeArea pageAnimEnter"
